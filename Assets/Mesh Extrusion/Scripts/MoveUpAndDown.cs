a68d88be1c884125a81375dc4134f1046cee1d04
Emanuel T <emanuel@insilico.uk>
2019-01-15 20:07:49 +0100

    Added all files

---
+++ Assets/Mesh Extrusion/Scripts/MoveUpAndDown.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpAndDown : MonoBehaviour
+{
+
+    void Update()
+    {
+        // Move the object upward in world space 1 unit/second.
+        transform.position = transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time/2, 0.7f),  transform.position.z);
+    }
+}
